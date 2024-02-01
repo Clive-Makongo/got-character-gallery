23bf321f4e1f4820b1014913288f8e1dab5abfb8
Clive Makongo <clivemakongo@gmail.com>
2024-02-01 19:18:39 +0000

    pushed React files

---
+++ src/tests/App.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import * as userEvent from '@testing-library/user-event';
+import App from '../App';
+
+it('should render the App', () => {
+    render(<App />);
+
+});
